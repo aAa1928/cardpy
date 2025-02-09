bc54b2c675fba8cb8cd1889c33abf53805f67087
Risheet Lenka <risheetlenka01@gmail.com>
2025-02-08 17:13:46 -0800

    updated enum values

--- cardpy/cardpy.py
+++ cardpy/cardpy.py
@@ -1,30 +1,37 @@
-from enum import Enum, auto
+from enum import Enum
 
 class Suit(Enum):
-    HEARTS = auto()
-    DIAMONDS = auto()
-    CLUBS = auto()
-    SPADES = auto()
+    HEARTS = '♥'
+    DIAMONDS = '♦'
+    CLUBS = '♣'
+    SPADES = '♠'
 
 class Rank(Enum):
-    TWO = auto()
-    THREE = auto()
-    FOUR = auto()
-    FIVE = auto()
-    SIX = auto()
-    SEVEN = auto()
-    EIGHT = auto()
-    NINE = auto()
-    TEN = auto()
-    JACK = auto()
-    QUEEN = auto()
-    KING = auto()
-    ACE = auto()
+    TWO = '2'
+    THREE = '3'
+    FOUR = '4'
+    FIVE = '5'
+    SIX = '6'
+    SEVEN = '7'
+    EIGHT = '8'
+    NINE = '9'
+    TEN = '10'
+    JACK = 'J'
+    QUEEN = 'Q'
+    KING = 'K'
+    ACE = 'A'
 
 class Card:
-    def __init__(self, rank: Rank, suit: Suit):
-        self.rank = rank
-        self.suit = suit
+    def __init__(self, _rank: Rank, _suit: Suit):
+        self.rank = _rank
+        self.suit = _suit
+
+    def __str__(self):
+        return f'{self.rank.value}{self.suit.value}'
 
     def __repr__(self):
         return f'Card({self.rank=}, {self.suit=})'
+    
+if __name__ == '__main__':
+    card = Card(Rank.ACE, Suit.SPADES)
+    print(card)
---
+++ cardpy/main.py
@@ -0,0 +1,1 @@
+from .cardpy import Rank, Suit, Card
