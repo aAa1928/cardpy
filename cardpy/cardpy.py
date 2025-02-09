d340a8820ebf474811050ea7f08010d44733cc10
Risheet Lenka <risheetlenka01@gmail.com>
2025-02-08 17:31:11 -0800

    added Color class

--- cardpy/cardpy.py
+++ cardpy/cardpy.py
@@ -20,11 +20,23 @@ class Rank(Enum):
     QUEEN = 'Q'
     KING = 'K'
     ACE = 'A'
+    JOKER = NotImplemented # TODO
+
+class Color(Enum):
+    RED = 'red'
+    BLACK = 'black'
 
 class Card:
     def __init__(self, _rank: Rank, _suit: Suit):
         self.rank = _rank
         self.suit = _suit
+        self.color = Color.RED if self.suit in [Suit.HEARTS, Suit.DIAMONDS] else Color.BLACK if self.suit in [Suit.SPADES, Suit.CLUBS] else None
+
+    def __eq__(self, other):
+        if not isinstance(other, Card):
+            return ValueError('Cannot compare Card with non-Card object')
+        
+        return self.rank == other.rank and self.suit == other.suit
 
     def __str__(self):
         return f'{self.rank.name} of {self.suit.name} {self.suit.value}'
@@ -33,6 +45,6 @@ class Card:
         return f'Card({self.rank=}, {self.suit=})'
     
 if __name__ == '__main__':
-    card = Card(Rank.ACE, Suit.SPADES)
-    print(card)
-    print(repr(card))
+    card1 = Card(Rank.ACE, Suit.SPADES)
+    card2 = Card(Rank.ACE, Suit.SPADES)
+    print(card1 == card2)
