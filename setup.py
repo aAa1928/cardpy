841deacf5db4f2c68990bf60fc11ebbe605f7d20
Risheet Lenka <risheetlenka@gmail.com>
2025-02-09 20:35:56 -0800

    Update setup.py
    fixed minor bug in name

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='A Python module for playing cards.',
     long_description=open('README.md').read() if os.path.exists('README.md') else '',
     long_description_content_type='text/markdown',
-    url='https://github.com/aAa1928/pycard',
+    url='https://github.com/aAa1928/cardpy',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
